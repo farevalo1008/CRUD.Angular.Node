bb52fb17514f90a0ab0958f124ce0cdb77cd7cb8
Fernando Arevalo <Fernando.Arevalo@credicard.com.ve>
2021-03-26 14:28:54 -0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule
